2cb19dedc4e75fd3cbfd92126b3ce149a87dbaff
PL-KRG <tomohisa.hiragi@gmail.com>
2025-04-06 19:04:14 +0900

    TRPG Systemを選択しキャラ作成にpost

--- trpg-remix-app/app/features/character/components/character.tsx
+++ trpg-remix-app/app/features/character/components/character.tsx
@@ -1,7 +1,12 @@
-import { Button } from '@mantine/core'
+import { Button, ComboboxItem } from '@mantine/core'
 import { useOutletContext } from '@remix-run/react'
 import axios from 'axios'
 import { CustomError } from '~/utils/customError'
+import { Select } from '@mantine/core'
+import gameSystemList from '~/static/gameSystemList.json'
+import { useState } from 'react'
+
+const gameSystemListID = gameSystemList.map(e => ({ value: e.ID, label: e.NAME }))
 
 export function Character() {
   const outletContextData = useOutletContext<{ data: any, cookie: string }>()
@@ -12,12 +17,22 @@ export function Character() {
     }
   const jwt = jwtCookie.split('=')[1]
 
+  const headers = {'Content-Type': 'application/json', Authorization: `Bearer ${jwt}`}
+
   async function clickHandler(){
     console.log('clicked!')
     const corsServerDomain = 'http://localhost:3000'
+
+    if (!TRPGSystemValue) {
+      console.log('no select TRPG System')
+      return
+    }
+
     try
     {
-      const res = await axios.post(`${corsServerDomain}/characters/create`, { TRPGName: 'Cthulhu' }, { headers: {'Content-Type': 'application/json', Authorization: `Bearer ${jwt}`}, withCredentials: true })
+      const res = await axios.post(`${corsServerDomain}/characters/create`,
+        { TRPGName: TRPGSystemValue.value },
+        { headers , withCredentials: true })
       console.log('--- res ---')
       console.log(res)
     }catch(Error)
@@ -26,12 +41,23 @@ export function Character() {
     }
   }
 
+  const [TRPGSystemValue, setTRPGSystemValue] = useState<ComboboxItem | null>(null)
+
   return (
     <>
       <div>
         Character create page
       </div>
       <Button onClick={clickHandler}>create character</Button>
+      <Select
+        label="TRPG System"
+        placeholder="Pick TRPG System"
+        data={gameSystemListID}
+        searchable
+        nothingFoundMessage="Nothing found..."
+        value={TRPGSystemValue ? TRPGSystemValue.value : null}
+        onChange={(_value, option) => setTRPGSystemValue(option)}
+      />
     </>
   )
 }
