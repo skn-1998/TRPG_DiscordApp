a1582c0c90c5a593bddb41a00d5c82fd92c6bad8
PL-KRG <tomohisa.hiragi@gmail.com>
2025-04-24 05:50:26 +0900

    generateColorsの改善 AppShell導入

--- trpg-remix-app/app/theme.ts
+++ trpg-remix-app/app/theme.ts
@@ -3,11 +3,12 @@ import generateColors from './utils/generateColors'
 
 const primaryShade = 5
 
-const mainColor: MantineColorsTuple = generateColors('#424242', primaryShade)
+const mainColor: MantineColorsTuple = generateColors('#3A3A3A', primaryShade)
 const accentColor: MantineColorsTuple = generateColors('#673AB7', primaryShade)
 const subColor: MantineColorsTuple = generateColors('#78b7b7', primaryShade)
 const complementaryColor: MantineColorsTuple = generateColors('#8ab73a', primaryShade)
 const subComplementaryColor: MantineColorsTuple = generateColors('#b77878', primaryShade)
+const bgColor: MantineColorsTuple = colorsTuple('#1E1E23')
 
 const theme = createTheme({
   primaryColor: 'main',
@@ -16,7 +17,8 @@ const theme = createTheme({
     accent: accentColor,
     sub: subColor,
     comp: complementaryColor,
-    subComp: subComplementaryColor
+    subComp: subComplementaryColor,
+    bg: bgColor
   },
   primaryShade
 })
--- trpg-remix-app/app/utils/generateColors.ts
+++ trpg-remix-app/app/utils/generateColors.ts
@@ -1,4 +1,4 @@
-import { darken, lighten } from '@mantine/core'
+import { darken, lighten, colorsTuple } from '@mantine/core'
 
 const rgba2hex = (rgba: string) => {
   const rgbaArray = rgba.replace(/^rgba?\(|\s+|\)$/g, '').split(',').map(e => Number(e))
@@ -6,25 +6,14 @@ const rgba2hex = (rgba: string) => {
   return '#' + rgbaArray.map(e => ('0' + e.toString(16)).slice(-2)).join('')
 }
 
-function generateColors (hex: string, primaryShade: number): [string, string, string, string, string, string, string, string, string, string] {
+function generateColors (hex: string, primaryShade: number) {
   const colors = Array.from({ length: 10 }, (_, index) => {
     const diff = primaryShade - index
     const alpha = Math.abs(diff / 10) + 0.03
     const rgba = diff >= 0 ? lighten(hex, alpha) : darken(hex, alpha)
     return rgba2hex(rgba)
   })
-  return [
-    `${colors[0]}`,
-    `${colors[1]}`,
-    `${colors[2]}`,
-    `${colors[3]}`,
-    `${colors[4]}`,
-    `${colors[5]}`,
-    `${colors[6]}`,
-    `${colors[7]}`,
-    `${colors[8]}`,
-    `${colors[9]}`,
-  ]
+  return colorsTuple(colors)
 }
 
 export default generateColors
