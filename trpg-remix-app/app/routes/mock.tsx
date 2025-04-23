811a3edb1779104c3dea0ce9e320c0c4ed680a95
PL-KRG <tomohisa.hiragi@gmail.com>
2025-04-24 05:50:26 +0900

    generateColorsの改善 AppShell導入

--- trpg-remix-app/app/routes/mock.tsx
+++ trpg-remix-app/app/routes/mock.tsx
@@ -1,11 +1,27 @@
 import { Outlet } from '@remix-run/react'
 import { MockButton } from '~/features/mock'
+import { AppShell, useMantineTheme, darken, lighten } from '@mantine/core'
 
 export default function MockIndex() {
+  const theme = useMantineTheme()
+  
   return (
     <>
-      <MockButton />
-      <Outlet />
+    <AppShell
+          header={{ height: 60 }}
+          navbar={{
+            width: 250,
+            breakpoint: 'sm',
+          }}
+          padding="md"
+    >
+      <AppShell.Header bg={darken(theme.colors.bg[0], 0.4)}></AppShell.Header>
+      <AppShell.Navbar p="md" bg={lighten(theme.colors.bg[0], 0.05)}>Navbar</AppShell.Navbar>
+      <AppShell.Main>
+        <MockButton />
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
     </>
   )
 }
