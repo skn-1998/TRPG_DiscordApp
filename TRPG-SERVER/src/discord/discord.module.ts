41813231c20da8a4750bbefac000c2d28d7c6471
skn-1998 <sekino.ara.ta@gmail.com>
2025-04-30 00:44:07 +0900

    module update

--- TRPG-SERVER/src/discord/discord.module.ts
+++ TRPG-SERVER/src/discord/discord.module.ts
@@ -5,22 +5,13 @@ import { EventsModule } from './events/events.module'
 import { CommandsModule } from './commands/commands.module'
 import { DiscordClientService } from './services/discord-client.service'
 import { CommandManagerService } from './services/command-manager.service'
-import { EventManagerService } from './services/event-manager.service'
 import { DiscordCommandRegistrationService } from './services/discord-command-registration.service'
-import { DiscordEventRegistrationService } from './services/discord-event-registration.service'
 import { CharacterModule } from '../domains/character/character.module'
 
 @Module({
   imports: [ConfigModule, forwardRef(() => EventsModule), CommandsModule, CharacterModule],
   controllers: [],
-  providers: [
-    DiscordService,
-    DiscordClientService,
-    CommandManagerService,
-    EventManagerService,
-    DiscordCommandRegistrationService,
-    DiscordEventRegistrationService
-  ],
-  exports: [DiscordService, DiscordClientService, CommandManagerService, EventManagerService]
+  providers: [DiscordService, DiscordClientService, CommandManagerService, DiscordCommandRegistrationService],
+  exports: [DiscordService, DiscordClientService, CommandManagerService]
 })
 export class DiscordModule {}
