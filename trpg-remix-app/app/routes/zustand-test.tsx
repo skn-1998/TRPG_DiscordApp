6501eaf3188d6f445137c32835731b305c719fc9
PL-KRG <tomohisa.hiragi@gmail.com>
2025-05-25 18:56:33 +0900

    zustand slice pattern

--- trpg-remix-app/app/routes/zustand-test.tsx
+++ trpg-remix-app/app/routes/zustand-test.tsx
@@ -1,14 +1,23 @@
-import useBoundStore from '~/store'
+import useStore from '~/store'
 import { Button } from '@mantine/core'
 
 export default function ZustandTest() {
-  const incrementCount = useBoundStore((state) => state.incrementCount)
-  const count = useBoundStore((state) => state.count)
+  const increment = useStore((state) => state.increment)
+  const decrement = useStore((state) => state.decrement)
+  const count = useStore((state) => state.count)
+
+  const setA = useStore((state) => state.setA)
+  const setB = useStore((state) => state.setB)
+  const abc = useStore((state) => state.abc)
 
   return (
     <>
       <p>{count}</p>
-      <Button onClick={() => incrementCount()}>increment</Button>
+      <Button onClick={() => increment()}>increment</Button>
+      <Button onClick={() => decrement()}>decrement</Button>
+      <p>{abc}</p>
+      <Button onClick={() => setA()}>setA</Button>
+      <Button onClick={() => setB()}>setB</Button>
     </>
   )
 }
