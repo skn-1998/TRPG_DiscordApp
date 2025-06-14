2b6cef72887d396b42e4fe129c238b4dc2260412
PL-KRG <tomohisa.hiragi@gmail.com>
2025-06-15 07:37:59 +0900

    temp

--- trpg-remix-app/app/routes/character+/edit.tsx
+++ trpg-remix-app/app/routes/character+/edit.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from '@remix-run/react'
 import { CharacterCreate } from '~/features/character'
+import { CharacterEditMock } from '~/features/character/components/characterEdit'
 
 export default function CharacterEdit() {
   return (
     <div>
       character edit
+      <CharacterEditMock />
       <Outlet />
     </div>
   )
