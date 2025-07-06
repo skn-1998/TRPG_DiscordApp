079a0ba3cee9509cea2abc19982b02f9a7f9caae
PL-KRG <tomohisa.hiragi@gmail.com>
2025-07-06 20:35:55 +0900

    flex table 仮

---
+++ trpg-remix-app/app/features/character/components/flexTable.tsx
@@ -0,0 +1,59 @@
+import styles from './flexTable.module.css'
+
+function BunchHeader() {
+  return (
+    <>
+      <div className={styles.bunch}>
+        <div className={styles.sideHeaderCell}></div>
+        <div className={styles.headerCell}>aaa</div>
+        <div className={styles.headerCell}>bbb</div>
+        <div className={styles.headerCell}>ccc</div>
+      </div>
+    </>
+  )
+}
+
+function Cell() {
+  return (
+    <>
+      <div className={styles.cell}>
+        <div className={styles.cellWrap}>
+          <div className={styles.cellWrapWrap}>
+            <input type="number" className={styles.cellInputNumber}></input>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function Bunch() {
+  return (
+    <>
+      <div className={styles.bunch}>
+        <div className={styles.sideHeaderCell}>zzz</div>
+        <Cell />
+        <Cell />
+        <Cell />
+      </div>
+    </>
+  )
+}
+
+export function FlexTable() {
+  return (
+    <>
+      <div className={styles.mainContainer}>
+        <div className={styles.tableContainer}>
+          <BunchHeader />
+          <Bunch />
+          <Bunch />
+          <Bunch />
+          <Bunch />
+          <Bunch />
+          <Bunch />
+        </div>
+      </div>
+    </>
+  )
+}
