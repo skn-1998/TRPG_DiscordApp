bba7a89f5334ac89c87c01c237166b42420a08da
PL-KRG <tomohisa.hiragi@gmail.com>
2025-08-09 23:26:27 +0900

    temp

--- trpg-remix-app/app/routes/character+/grid.tsx
+++ trpg-remix-app/app/routes/character+/grid.tsx
@@ -1,6 +1,7 @@
 import { FlexTable } from '~/features/character/components/flexTable'
 import { FlexTable2 } from '~/features/character/components/flexTable2'
 import { GridTest } from '~/features/character/components/gridTest'
+import { StatusTable } from '~/features/character/components/statusTable'
 
 export default function GridStudy() {
   return (
@@ -8,6 +9,7 @@ export default function GridStudy() {
       {/* <GridTest /> */}
       <FlexTable />
       <FlexTable2 />
+      <StatusTable />
     </>
   )
 }
