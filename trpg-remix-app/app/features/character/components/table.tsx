182e02c20188c0f926d94e1bce946bd99ae08e21
PL-KRG <tomohisa.hiragi@gmail.com>
2025-08-11 17:33:18 +0900

    side input test

--- trpg-remix-app/app/features/character/components/table.tsx
+++ trpg-remix-app/app/features/character/components/table.tsx
@@ -1,4 +1,4 @@
-import { ChangeEventHandler } from 'react'
+import { ChangeEvent, ChangeEventHandler, ReactNode, useState } from 'react'
 import styles from './flexTable2.module.css'
 
 type TextCellProps = {
@@ -38,18 +38,54 @@ function InputCell({ value, changeHandler }: InputCellProps) {
   )
 }
 
-type SideGroupProps = {
-  sideHeaderText: string
+function SideInputCell({ value, changeHandler }: InputCellProps) {
+  return (
+    <>
+      <div className={`${styles.textCell} ${styles.textCellCommon}`}>
+        <div className={styles.textCellInner}>
+          <div className={styles.sideInputWrap}>
+            <input type="text" className={styles.inputText} value={value} onChange={changeHandler} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-function SideGroup({ sideHeaderText }: SideGroupProps) {
+type SideGroupProps = {
+  sideHeaderText: string
+  sideCellGroup: ReactNode
+}
+
+function SideGroup({ sideHeaderText, sideCellGroup }: SideGroupProps) {
   return (
     <>
       <div className={styles.textGroup}>
         <div className={`${styles.textHeaderCell} ${styles.inputCellCommon}`}>
           <b>{sideHeaderText}</b>
         </div>
+        {sideCellGroup}
       </div>
+    </>
+  )
+}
+
+function SideCellGroup() {
+  const [value, setValue] = useState('運転()')
+
+  const changeHandler = (e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)
+
+  return (
+    <>
+      <SideInputCell value={value} changeHandler={changeHandler} />
+    </>
+  )
+}
+
+export function TestSideGroup() {
+  return (
+    <>
+      <SideGroup sideHeaderText="hello" sideCellGroup={<SideCellGroup />}></SideGroup>
     </>
   )
 }
